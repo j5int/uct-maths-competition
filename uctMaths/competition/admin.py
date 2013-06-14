16a7a8ede001aba2f0688480c9f9e90e9844f97b
aureliad <aureliad>
2013-06-14 14:27:06 +0200

    [ADD] added admin.py to get the admin to register the models

---
+++ uctMaths/competition/admin.py
@@ -0,0 +1,7 @@
+from competition.models import *
+from django.contrib import admin
+
+admin.site.register(User)
+admin.site.register(SchoolUser)
+admin.site.register(School)
+admin.site.register(SchoolStudent)
