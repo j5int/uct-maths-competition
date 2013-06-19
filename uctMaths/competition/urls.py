b282f3050d69437cde5a1f44984a9972e05e3ec9
Amber Goldberg <ambertonig@gmail.com>
2013-06-19 13:05:20 +0200

    The testing out the forms, and how they work....still in progress :)

--- uctMaths/competition/urls.py
+++ uctMaths/competition/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import patterns, url
 from competition import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.tester, name='tester'),
+  url(r'^content/', views.content, name='content'),
+  url(r'^main/', views.main, name='main'),
+  url(r'^regStudent/', views.regStudent, name='regStudent'),
+  url(r'^search-form/$', views.search_form),
+  url(r'^search/$', views.search),
   #url(r'^$', views.current_datetime, name='current_datetime')
   url(r'^$', views.index, name='index'),
 
