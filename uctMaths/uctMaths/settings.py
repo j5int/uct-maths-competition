1893b3178ed05e52dbd07a50067234b2dec6917a
aureliad <aureliad>
2013-06-20 20:07:11 +0200

    [FIX] fixed my setup so that it is no longer broken.

--- uctMaths/uctMaths/settings.py
+++ uctMaths/uctMaths/settings.py
@@ -120,7 +120,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/amber/Desktop/uct-maths/uctMaths/competition/interface',
+    '/home/aureliad/Desktop/uct-maths/uctMaths/competition/interface',
 
  )
 
