3dada82edb0dd9dd1ad3ee056db0b84535d54cbb
frank-retief <frankretief@gmail.com>
2013-12-19 16:40:41 +0200

    [UPDATE] Final commit for the day. Initial work on allocation algorithm (seems to be working when there are enough venues for students). TO BE IMPROVED.

--- uctMaths/competition/compadmin.py
+++ uctMaths/competition/compadmin.py
@@ -2,6 +2,8 @@
 # administration.
 from competition.models import SchoolStudent, School, Invigilator, Venue, ResponsibleTeacher 
 from datetime import date
+import operator
+
 
 #A few administration constants and associated methods to be used around the website.
 
@@ -25,12 +27,82 @@ def closingDate():
     return str(comp_closingdate.day) + '/' + str(comp_closingdate.month)  + '/' + str(comp_closingdate.year)
 
 #TODO: Auto-venue allocation algorithm
-#def auto_allocate():
-#    print 'auto_allocate'
-#    student_list = SchoolStudent.objects.filter(school = assigned_school)
-#    individual_list, pair_list = processGrade(student_list) #processGrade is defined below this method
-#    #invigilator_list = Invigilator.objects.filter(school = assigned_school)
-#    responsible_teacher = ResponsibleTeacher.objects.filter(school = assigned_school)
+def auto_allocate():
+    print 'auto_allocate'
+    venue_deallocate()
+    student_list = SchoolStudent.objects.all().order_by('grade') #Order by grade (ASCENDING)
+    venue_list = Venue.objects.all().order_by('-seats') #Order by seats (DECENDING)
+    
+    grade_bucket = gradeBucket(student_list)
+
+#For each venue
+    #calculate the "most filling" grade
+    #If two are equal, take the grade with the most students left
+    fullness = {8:1.0, 9:1.0, 10:1.0, 11:1.0, 12:1.0}
+
+    for venue in venue_list:
+        
+        for key in range(8,13):
+            fullness[key] = len(grade_bucket[key])*1.0/venue.seats
+            print 'L[',key,']=', len(grade_bucket[key]), ' ',
+
+        #Find grade that maximises the use of the venue (highest percentage)
+        max_key = max(fullness.iteritems(), key=operator.itemgetter(1))[0]
+        print 'KEY:',max_key,
+        #Pop students until the bucket is empty or the venue is full
+        while grade_bucket[max_key] and venue.pairs*2+venue.individuals < venue.seats:
+            #TODO: Handle keeping-pairs-together
+
+            #print 'while ', len(grade_bucket[max_key]),' and ',venue.pairs*2,'+',venue.individuals,' <= ',venue.seats
+            student = grade_bucket[max_key].pop(0) #Better logic here!
+            student.venue = venue
+            
+            if student.paired: #find the second of the pair
+                try:
+                    paired_student = grade_bucket[max_key].pop(0) #Better logic here!
+                except IndexError:
+                    print '[Pair error]',
+
+                paired_student.venue = venue.code
+                paired_student.save()
+                venue.pairs += 1
+            else:
+                venue.individuals += 1
+
+            student.save()
+        print ' V:',venue.code,' VP:',venue.pairs,'VI:',venue.individuals,''
+        venue.grade = max_key
+        venue.save()
+    
+    #Check to see if there are too many students
+    max_key = max(fullness.iteritems(), key=operator.itemgetter(1))[0]
+    if max_key != 0:
+        print 'Unassigned students!'
+
+def venue_deallocate():
+    student_list = SchoolStudent.objects.all().order_by('grade')
+    venue_list = Venue.objects.all().order_by('-seats') #Order by seats (DECENDING)
+    
+    for student in student_list:
+        student.venue = ''
+        student.save()
+
+    for venue in venue_list:
+        venue.pairs = 0
+        venue.individuals = 0
+        venue.save()
+
+#Would this be more/less efficient than query-based binning?
+# I need to be able to access length of list, each student's details.
+def gradeBucket(student_list):
+    """ Secondary function for  """
+    grade_bucket = { 8 : [] , 9 :  [] , 10 :  [] , 11 : [] , 12 : [] }
+    try:
+        for student in student_list:
+            grade_bucket[student.grade].append(student)
+    except IndexError:
+        print 'Index Error'
+    return grade_bucket
 
 #Used in confirmation.py and views.py
 def processGrade(student_list):
