da0767fa0a95573821eaa77aa857dff4b7396baa
frank-retief <frankretief@gmail.com>
2014-01-02 14:14:07 +0200

    [UPDATE] Added an option in VenuesAdmin to export venue rosters as sheets in an xls doc which is downloaded on click.

--- uctMaths/competition/compadmin.py
+++ uctMaths/competition/compadmin.py
@@ -3,7 +3,12 @@
 from competition.models import SchoolStudent, School, Invigilator, Venue, ResponsibleTeacher 
 from datetime import date
 import operator
-
+import glob
+import csv
+import xlwt
+import os
+from django.core.servers.basehttp import FileWrapper
+from django.http import HttpResponse
 
 #A few administration constants and associated methods to be used around the website.
 
@@ -119,3 +124,73 @@ def processGrade(student_list):
         print 'Index Error'
     return individual_list, pair_list
 
+#Export venue lists to workbook.
+#See http://scienceoss.com/write-excel-files-with-python-using-xlwt/
+
+def output_register(venue_list):
+    """Output selected venues (arg) as sheets on an xls"""
+
+    output_workbook = xlwt.Workbook()
+    student_header = ['Reference No.','Surname, first name(s)', 'School']
+
+    #Generate a list of unallocated invigilators and students
+    summary_sheet = output_workbook.add_sheet('Unallocated')
+    summary_sheet.write(0,0,'Summary page')
+
+    venue_h = ['Venue', 'Building', 'Grade', 'Available seats', 'Occupied seats','Allocation']
+
+    for index, header in enumerate(venue_h):
+        summary_sheet.write(1, index, header)
+
+    venue_list.order_by('grade')
+
+    for v_index, venue in enumerate(venue_list):
+        summary_sheet.write(v_index+2,0,str(venue.code))
+        summary_sheet.write(v_index+2,1,str(venue.building))
+        summary_sheet.write(v_index+2,2,str(venue.grade))
+        summary_sheet.write(v_index+2,3,str(venue.seats))
+        summary_sheet.write(v_index+2,4,str(venue.occupied_seats))
+
+        if venue.allocated_to_pairs:
+            summary_sheet.write(v_index+2,5,'Pairs')
+        else:
+            summary_sheet.write(v_index+2,5,'Individuals')
+
+    #Create a new sheet for each venue
+    for venue in venue_list:
+        student_list = SchoolStudent.objects.all().filter(venue=venue.code)
+        #invigilator_list = Invigilator.objects.all().filter(venue=venue.code)
+
+        if student_list: #If the list is not empty ("More efficient than len()" - Django documentation) 
+            venue_sheet = output_workbook.add_sheet(str(venue.code))
+            venue_header = [ #Heading for each sheet.
+                            'Venue:', str(venue.code), 
+                            'Building: ', str(venue.building),
+                            'Grade:', str(venue.grade),
+                            'Occupancy:', str(venue.occupied_seats)+'/'+str(venue.seats),
+                            'Allocation:', 'Pairs' if venue.allocated_to_pairs else 'Individuals'
+                            ]
+
+            for index in range(0,5):
+                venue_sheet.write(index,0, venue_header[index*2])
+                venue_sheet.write(index,1, venue_header[index*2+1])
+
+            for h_index, word in enumerate(student_header):
+                venue_sheet.write(6,h_index,student_header[h_index])
+
+            for s_index, student in enumerate(student_list):
+                venue_sheet.write(s_index+7,0,str(student.reference))
+                venue_sheet.write(s_index+7,1,str(student.surname)+', '+str(student.firstname))
+                venue_sheet.write(s_index+7,2,str(student.school))
+
+        else:
+            pass #Venue is empty - no point making a sheet for it...
+            #print 'Empty venue!'
+
+    try:
+        response = HttpResponse(mimetype='application/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename=venue_register.xls'
+        output_workbook.save(response)
+        return response
+    except IndexError:
+        print 'No full sheets!'
