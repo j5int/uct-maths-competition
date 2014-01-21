da0c62f341acd5509b2e4328549c0059d97cb4e4
frank-retief <frankretief@gmail.com>
2014-01-21 11:10:35 +0200

    [UPDATE] Multi-page report gen working

--- uctMaths/competition/admin.py
+++ uctMaths/competition/admin.py
@@ -31,7 +31,7 @@ class SchoolAdmin(ImportExportModelAdmin):
 	list_display = ('key', 'name', 'language', 'address','phone','fax','contact','email','assigned_to', 'score', 'rank', 'entered') ##Which columns should be kept here? 
 	search_fields = ['name']
 	resource_class = SchoolResource
-	actions = ['remove_user_associations', 'output_schooltaglist', 'assign_school_ranks', 'school_summary','print_school_confirmations', 'update_school_entry_status','generate_school_reports']
+	actions = ['remove_user_associations', 'output_schooltaglist', 'assign_school_ranks', 'school_summary','print_school_confirmations', 'update_school_entry_status','generate_school_reports','generate_multi_school_reports']
     #import school dataset
 	#Expects csv (comma-separated) file with the first line being:
     #id,name,key,language,address,phone,fax,contact,entered,score,email,assigned_to(leave blank),registered_by
@@ -59,6 +59,9 @@ class SchoolAdmin(ImportExportModelAdmin):
 
 	def generate_school_reports(self, request, queryset):
 	    return compadmin.print_school_reports(request, queryset)
+
+	def generate_multi_school_reports(self, request, queryset):
+	    return compadmin.multi_reportgen(request, queryset)
 
 	output_schooltaglist.short_description = 'Download school tags for selected school(s)'
 	remove_user_associations.short_description = 'Remove associated users to selected school(s)'
