f923e99a3a64083fea2086ca43004783ec0d9304
juliank <juliank@j5int.com>
2017-01-30 10:59:09 +0200

    manually added location column to competition_school,competition_schoolstudent,competition_venue,competition_invigilator,competition_schoolstudentarchive,competition_invigilatorarchive since Django 1.6 does not support migrations out of the box. Also updated readme with instructions on how to setup the UCT Maths Competition site on a local development machine

---
+++ uctMaths/scripts/add_location_column_27012017.sql
@@ -0,0 +1,45 @@
+--$psql  <DBNAME> -a -f /<LOCATION>/add_location_column_27012017.sql
+
+DO $$
+	BEGIN
+		BEGIN
+			ALTER TABLE competition_school ADD COLUMN  "Location" varchar(3) NOT NULL DEFAULT '-';
+		EXCEPTION
+			WHEN duplicate_column THEN RAISE NOTICE 'column Location already exists in competition_school.';
+		END;
+		BEGIN
+			ALTER TABLE competition_schoolstudent ADD COLUMN  "Location" varchar(3) NOT NULL DEFAULT '-';
+		EXCEPTION
+			WHEN duplicate_column THEN RAISE NOTICE 'column Location already exists in competition_schoolstudent.';
+		END;
+		BEGIN
+			ALTER TABLE competition_venue ADD COLUMN  "Location" varchar(3) NOT NULL DEFAULT '-';
+		EXCEPTION
+			WHEN duplicate_column THEN RAISE NOTICE 'column Location already exists in competition_venue.';
+		END;
+		BEGIN
+			ALTER TABLE competition_invigilator ADD COLUMN  "Location" varchar(3) NOT NULL DEFAULT '-';
+		EXCEPTION
+			WHEN duplicate_column THEN RAISE NOTICE 'column Location already exists in competition_invigilator.';
+		END;
+		BEGIN
+			ALTER TABLE competition_schoolstudentarchive ADD COLUMN  "Location" varchar(3) NOT NULL DEFAULT '-';
+		EXCEPTION
+			WHEN duplicate_column THEN RAISE NOTICE 'column Location already exists in competition_schoolstudentarchive.';
+		END;
+		BEGIN
+			ALTER TABLE competition_invigilatorarchive ADD COLUMN  "Location" varchar(3) NOT NULL DEFAULT '-';
+		EXCEPTION
+			WHEN duplicate_column THEN RAISE NOTICE 'column Location already exists in competition_invigilatorarchive.';
+		END;
+
+		BEGIN
+			ALTER TABLE competition_school ALTER COLUMN  "Location" DROP DEFAULT;
+			ALTER TABLE competition_schoolstudent ALTER COLUMN "Location" DROP DEFAULT;
+			ALTER TABLE competition_venue ALTER COLUMN "Location" DROP DEFAULT;
+			ALTER TABLE competition_invigilator ALTER COLUMN "Location" DROP DEFAULT;
+			ALTER TABLE competition_schoolstudentarchive ALTER COLUMN "Location" DROP DEFAULT;
+			ALTER TABLE competition_invigilatorarchive ALTER COLUMN "Location" DROP DEFAULT;
+		END;
+	END;
+$$
