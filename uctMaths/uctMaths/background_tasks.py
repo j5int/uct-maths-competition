df660634d0533d1e98b0ce1db03e7a8c003b7d20
ralph.mcdougall@hexagon.com <Tjcrqu1ckly>
2021-01-26 16:24:31 +0200

    Working on sending document

--- uctMaths/uctMaths/background_tasks.py
+++ uctMaths/uctMaths/background_tasks.py
@@ -1,5 +1,6 @@
 from background_task import background
 
+import ho.pisa as pisa
 import sys
 sys.path.append("../")
 
@@ -7,4 +8,8 @@ sys.path.append("../")
 def bg_generate_school_answer_sheets(schoolID):
     print("Processing school with ID: " + str(schoolID))
     from competition.compadmin import generate_zipped_school_answer_sheets_from_id
-    print(generate_zipped_school_answer_sheets_from_id(schoolID))
+    from competition.reports import send_confirmation
+    path = generate_zipped_school_answer_sheets_from_id(schoolID)
+
+    pdf = pisa.pisaDocument("../" + path)
+    send_confirmation(None, pdf, "test")
