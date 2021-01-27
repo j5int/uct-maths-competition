5ae1c12b25f47e9947a4686d4ac3ec4a3a90eae1
ralph.mcdougall@hexagon.com <Tjcrqu1ckly>
2021-01-27 08:54:45 +0200

    Removed default values

--- uctMaths/scripts/add_report_date_validation_columns.sql
+++ uctMaths/scripts/add_report_date_validation_columns.sql
@@ -3,17 +3,17 @@
 DO $$
 	BEGIN
 		BEGIN
-			ALTER TABLE competition_competition ADD COLUMN  "prizegiving_date" DATE NOT NULL DEFAULT '9999-12-31';
+			ALTER TABLE competition_competition ADD COLUMN  "prizegiving_date" DATE;
 		EXCEPTION
 			WHEN duplicate_column THEN RAISE NOTICE 'column prizegiving_date already exists in competition_competition.';
 		END;
 		BEGIN
-			ALTER TABLE competition_responsibleteacher ADD COLUMN  "Report_downloaded" DATE NOT NULL DEFAULT '9999-12-31';
+			ALTER TABLE competition_responsibleteacher ADD COLUMN  "Report_downloaded" DATE;
 		EXCEPTION
 			WHEN duplicate_column THEN RAISE NOTICE 'column Report_downloaded already exists in competition_responsibleteacher.';
 		END;
 		BEGIN
-			ALTER TABLE competition_responsibleteacher ADD COLUMN  "Answer_sheet_downloaded" DATE NOT NULL DEFAULT '9999-12-31';
+			ALTER TABLE competition_responsibleteacher ADD COLUMN  "Answer_sheet_downloaded" DATE;
 		EXCEPTION
 			WHEN duplicate_column THEN RAISE NOTICE 'column Answer_sheet_downloaded already exists in competition_responsibleteacher.';
 		END;
