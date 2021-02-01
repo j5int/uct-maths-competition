ea63d33ebdf2c7abbe44bac9db32a8505603e353
ralph.mcdougall@hexagon.com <Tjcrqu1ckly>
2021-02-01 16:32:10 +0200

    Add report email clearing

--- uctMaths/scripts/clear_emails_sent_01022021.sql
+++ uctMaths/scripts/clear_emails_sent_01022021.sql
@@ -2,6 +2,10 @@ DO $$
     BEGIN
         BEGIN
             UPDATE competition_school
+                SET report_emailed = '1971-01-01 00:00:00';
+        END;
+        BEGIN
+            UPDATE competition_school
                 SET answer_sheets_emailed = '1971-01-01 00:00:00';
         END;
     END;
