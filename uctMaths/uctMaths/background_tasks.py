c2653726079c52c57a81273d9a031c6985b208ca
ralph.mcdougall@hexagon.com <ralph.mcdougall@hexagon.com>
2021-02-16 12:16:04 +0200

    Changed PDF name

--- uctMaths/uctMaths/background_tasks.py
+++ uctMaths/uctMaths/background_tasks.py
@@ -81,7 +81,7 @@ def bg_generate_as_grade_distinction(grade, paired):
         for pos, student in enumerate(batch):
             html += get_student_answer_sheet(None, student)
 
-        filename = "generated_grade_answer_sheets/%s answer sheets - grade %d - %d of %d .pdf" % ("Pair" if paired else "Individual",
+        filename = "generated_grade_answer_sheets/%s answer sheets - grade %d - %d of %d.pdf" % ("Pair" if paired else "Individual",
                                                                     grade, batch_no + 1, len(batches))
         if not os.path.exists("generated_grade_answer_sheets"):
             os.mkdir("generated_grade_answer_sheets")
