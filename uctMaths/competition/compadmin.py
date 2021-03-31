73fb787d3a161fa1440f8f5d16e116cd6eec75f4
David Fraser <david.fraser@hexagon.com>
2021-03-31 11:00:25 +0200

    Remove nasty butten that clears out all the addresses and phone numbers

--- uctMaths/competition/compadmin.py
+++ uctMaths/competition/compadmin.py
@@ -376,14 +376,6 @@ def remove_user_assoc(school_list):
         school.assigned_to = None
         school.save()
 
-def remove_emails_addresses():
-    """ Remove all addresses and school phone numbers from the database """
-    school_list = School.objects.all()
-    for school in school_list:
-        school.address = ""
-        school.phone = ""
-        school.save()
-
 #Called by admin to generate formatted 'tag list' for selected schools
 def output_schooltaglists(school_list):
     """ Generate the tags for a School QuerySet. Served as a single text file in HttpResponse. """
