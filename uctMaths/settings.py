e88789c0dcb382be3616b0f956b85a89d997285f
Steve Hofmeyr <steve.hofmeyr@hexagon.com>
2024-01-24 09:27:36 +0200

    add static root

--- uctMaths/settings.py
+++ uctMaths/settings.py
@@ -82,7 +82,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticroot/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -245,4 +245,12 @@ LOGGING = {
             'level': 'ERROR'
        },
     }
+}
+
+
+Q_CLUSTER = {
+    'redis': {
+        'host': 'cpt-xshofmeyr01.ingrnet.com',
+        'port': 6379,
+        'db': 0, }
 }
